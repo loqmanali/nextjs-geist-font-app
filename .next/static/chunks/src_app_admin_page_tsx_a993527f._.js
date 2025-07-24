43f7ce88e23a1f0b35dd6b8adb18e9288f780c4d
Loqman Ali <59010033+loqmanali@users.noreply.github.com>
2025-07-24 22:54:08 +0700

    Add .next/static/chunks/src_app_admin_page_tsx_a993527f._.js 1753372447052

---
+++ .next/static/chunks/src_app_admin_page_tsx_a993527f._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_a0d1645d._.js",
+  "static/chunks/node_modules_zod_lib_index_mjs_ee760afb._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_lodash_20634561._.js",
+  "static/chunks/node_modules_recharts_es6_f4354cdd._.js",
+  "static/chunks/node_modules_@radix-ui_58c2b3c6._.js",
+  "static/chunks/node_modules_@floating-ui_9ec1fa39._.js",
+  "static/chunks/node_modules_4f08e024._.js"
+],
+    source: "dynamic"
+});
